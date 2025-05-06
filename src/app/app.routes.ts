687304e161fc30b9814ea253b721e3166c0cf14d
LanaNew <l4n.steen@gmail.com>
2025-05-06 17:13:19 +0400

    angProject4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', redirectTo:  'home', pathMatch: "full"},
+    {path: "home", component: HomeComponent},
+    {path: "about", component: AboutComponent},
+    {path: "**", component: ErrorComponent}
+];
